f842933f2534ce707db1269dc5ea98f2305631cf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# How to use external dictionaries to check spelling (SpellCheckerHelper technique)
+
+
+<p>This example illustrates the use of external OpenOffice dictionaries for German language to check spelling of the TextBox content. The <strong>SpellCheckerHelper</strong> object provides access to the spell checker. <br />
+The dictionary has been converted to UTF-8 prior to use because SilverLight technology does not allow encodings other than UTF-8 and Unicode. To convert a dictionary, you can use the utility attached to the <a href="https://www.devexpress.com/Support/Center/p/K18404">K18404</a> article.<br />
+Note: when adding new dictionaries to this project, do not forget to specify for them Compile Action = Content.</p><p><i>NB</i>: <strong>DevExpress.XtraRichEdit.Commands.SpellCheckerHelper</strong> class is obsolete starting with version v2010 vol.2 . For a recommended technique, review the following article:<br />
+<a href="https://www.devexpress.com/Support/Center/p/E2306">E2306: How to use external dictionaries to check spelling (via SpellChecker() in code)</a>.</p>
+
+<br/>
+
+
---
+++ VB/GetStartedSpellChecker/MainPage.xaml.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports DevExpress.XtraSpellChecker
+Imports System.Windows.Resources
+Imports System.IO
+Imports DevExpress.XtraRichEdit.Commands
+Imports System.Globalization
+
+Namespace GetStartedSpellChecker
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			SpellCheckerHelper.SpellChecker.Check(tb)
+		End Sub
+
+		Private Sub btn_DictLoad_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			tb.IsEnabled = False
+			LayoutRoot.Cursor = Cursors.Wait
+
+			Dim dict As New SpellCheckerOpenOfficeDictionary()
+			Dim ResourceStreamDic As StreamResourceInfo = App.GetResourceStream(New Uri("Dictionary/de_DE.dic", UriKind.Relative))
+			Dim dicStream As Stream = ResourceStreamDic.Stream
+			Dim ResourceStreamAff As StreamResourceInfo = App.GetResourceStream(New Uri("Dictionary/de_DE.aff", UriKind.Relative))
+			Dim affStream As Stream = ResourceStreamAff.Stream
+			Dim ResourceStreamAlpha As StreamResourceInfo = App.GetResourceStream(New Uri("Dictionary/german_alphabet.txt", UriKind.Relative))
+			Dim alphaStream As Stream = ResourceStreamAlpha.Stream
+			dict.Culture = New CultureInfo("de-DE")
+			dict.LoadFromStream(dicStream, affStream, alphaStream)
+			SpellCheckerHelper.SpellChecker.Dictionaries.Add(dict)
+			SpellCheckerHelper.SpellChecker.Culture = New CultureInfo("de-DE")
+
+			tb.IsEnabled = True
+			LayoutRoot.Cursor = Cursors.Arrow
+		End Sub
+
+	End Class
+
+End Namespace
